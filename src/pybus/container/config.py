349bffd14753a1542c29e79b6df4a3f359621cf8
莊欽淯 <61406288+chinyue1910@users.noreply.github.com>
2026-04-14 13:24:50 +0800

    feat: add ApplicationSettings model for database and Kafka configuration management

--- src/pybus/container/config.py
+++ src/pybus/container/config.py
@@ -1,11 +1,19 @@
 from typing import ClassVar, Literal
 
+from pydantic import PostgresDsn, computed_field
+from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class ApplicationSettings(BaseSettings):
     DATABASE_TYPE: Literal["sqlalchemy"] = "sqlalchemy"
-    DATABASE_URL: str = ""
+
+    POSTGRES_SERVER: str = "postgres"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str = "acmepoint"
+    POSTGRES_PASSWORD: str = "93614849"
+    POSTGRES_DB: str = "ACMEPOINT"
+
     KAFKA_BOOTSTRAP_SERVERS: str = "localhost:9092"
 
     model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
@@ -13,3 +21,19 @@ class ApplicationSettings(BaseSettings):
         env_file_encoding="utf-8",
         extra="ignore",
     )
+
+    @computed_field
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        return PostgresDsn(
+            str(
+                MultiHostUrl.build(
+                    scheme="postgresql+psycopg",
+                    username=self.POSTGRES_USER,
+                    password=self.POSTGRES_PASSWORD,
+                    host=self.POSTGRES_SERVER,
+                    port=self.POSTGRES_PORT,
+                    path=self.POSTGRES_DB,
+                )
+            )
+        )
